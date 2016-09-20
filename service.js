60af18cdf7e8daad5432695a915c96a642125e5a
Alex <alex_sobko.94@mail.ru>
2016-09-21 00:04:06 +0300

    Добавлено формирование вариационного ряда. Не работает вывод в таблицу. Не удаляются одинаковые значения.

--- service.js
+++ service.js
@@ -1,13 +1,24 @@
 
+var res;
+
 function loadFile(files) {
-    alert('Alex LOH');
-    var file = files[0];
-    var reader = new FileReader();
+    var file = files[0],
+        reader = new FileReader();
 
     reader.onload = function (e) {
-        var p = document.createElement("p");
-        p.textContent = e.target.result;
-        document.body.appendChild(p);
+        res = e.target.result;
+        res = res.split("\n");
+        for (var i = 0, len = res.length; i < len; i++) {
+            $("#table").append("<tr><td>" + (i+1) + "</td><td>" + res[i] + "</td></tr>");
+        }
     };
     reader.readAsText(file);
 }
+
+$("#buildVarRow").click(function() {
+    debugger;
+    res = res.sort();
+    for (var i = 0, len = res.length; i < len; i++) {
+        $("#tr" + i).append("<td>" + res[i] + "</td>");
+    }
+});
