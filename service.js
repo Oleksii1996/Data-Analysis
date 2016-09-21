380ee4892f72246b7b6d12f09bbd16af11b78dcb
Alex <alex_sobko.94@mail.ru>
2016-09-22 00:10:09 +0300

    Строится вариационный ряд. Формируется таблица значений, кроме столбца значений емпирической функции распределения.

--- service.js
+++ service.js
@@ -1,24 +1,62 @@
-
-var res;
+// data - исходный массив данных(выборка), dimension - размерность выборки
+var data, dimension;
 
 function loadFile(files) {
     var file = files[0],
         reader = new FileReader();
 
     reader.onload = function (e) {
-        res = e.target.result;
-        res = res.split("\n");
-        for (var i = 0, len = res.length; i < len; i++) {
-            $("#table").append("<tr><td>" + (i+1) + "</td><td>" + res[i] + "</td></tr>");
-        }
+        data = e.target.result;
+        data = data.split("\n");
+
+        buildTable();
     };
     reader.readAsText(file);
 }
 
-$("#buildVarRow").click(function() {
-    debugger;
-    res = res.sort();
-    for (var i = 0, len = res.length; i < len; i++) {
-        $("#tr" + i).append("<td>" + res[i] + "</td>");
+// перестраиваем исходную выборку в вариационный ряд вида data[варианта][частота варианты]
+// выводим полученные данные в таблицу
+function buildTable() {
+
+    var tmp, tmpArr = [], j;
+
+    // копируем и храним исходную выборку для красивого вывода в таблицу
+    for (var i = 0, len = data.length; i < len; i++) {
+        tmpArr.push(data[i]);
     }
-});
+
+    dimension = data.length;
+
+    data = data.sort();
+
+    // перестраиваем выборку в вариационный ряд
+    for (var i = 0; i < data.length; i++) {
+        tmp = data[i];
+        data[i] = [];
+        data[i].push(tmp);
+        data[i].push(1);
+
+        // убираем найденные равные значения
+        j = i + 1;
+        while (j < data.length) {
+            if (data[i][0] == data[j]) {
+                data.splice(j, 1);
+                data[i][1]++;
+                continue;
+            } else {
+                j++;
+            }
+        }
+    }
+
+    // выводим результаты в таблицу
+    for (var i = 0, len = tmpArr.length; i < len; i++) {
+        if (i < data.length) {
+            $("#table").append("<tr><td>" + (i + 1) + "</td><td>" + tmpArr[i] + "</td>"
+            + "<td>" + data[i][0] + "</td>" + "<td>" + data[i][1] + "</td>" + "<td>"
+                + (data[i][1] / dimension) + "</td></tr>");
+        } else {
+            $("#table").append("<tr><td>" + (i + 1) + "</td><td>" + tmpArr[i] + "</td></tr>");
+        }
+    }
+}
