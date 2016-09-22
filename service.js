1afc6d01018a4cdba6834a82ce11dd165c1fff9a
Alex <alex_sobko.94@mail.ru>
2016-09-22 23:12:47 +0300

    Начато добавление разбиение вариационного ряда на классы. Пропадает контент.

--- service.js
+++ service.js
@@ -1,62 +1,90 @@
-// data - исходный массив данных(выборка), dimension - размерность выборки
-var data, dimension;
+(function() {
 
-function loadFile(files) {
-    var file = files[0],
-        reader = new FileReader();
+    // data - исходный массив данных(выборка), dimension - размерность выборки
+    var data, dimension;
 
-    reader.onload = function (e) {
-        data = e.target.result;
-        data = data.split("\n");
+    window.loadFile = function(files) {
+        var file = files[0],
+            reader = new FileReader();
 
-        buildTable();
-    };
-    reader.readAsText(file);
-}
+        reader.onload = function (e) {
+            data = e.target.result;
+            data = data.split("\n");
 
-// перестраиваем исходную выборку в вариационный ряд вида data[варианта][частота варианты]
-// выводим полученные данные в таблицу
-function buildTable() {
-
-    var tmp, tmpArr = [], j;
-
-    // копируем и храним исходную выборку для красивого вывода в таблицу
-    for (var i = 0, len = data.length; i < len; i++) {
-        tmpArr.push(data[i]);
+            buildTable();
+        };
+        reader.readAsText(file);
     }
 
-    dimension = data.length;
+    // перестраиваем исходную выборку в вариационный ряд вида data[варианта][частота варианты]
+    // выводим полученные данные в таблицу
+    function buildTable() {
 
-    data = data.sort();
+        var tmp, tmpArr = [], j;
 
-    // перестраиваем выборку в вариационный ряд
-    for (var i = 0; i < data.length; i++) {
-        tmp = data[i];
-        data[i] = [];
-        data[i].push(tmp);
-        data[i].push(1);
+        // копируем и храним исходную выборку для красивого вывода в таблицу
+        for (var i = 0, len = data.length; i < len; i++) {
+            tmpArr.push(data[i]);
+        }
 
-        // убираем найденные равные значения
-        j = i + 1;
-        while (j < data.length) {
-            if (data[i][0] == data[j]) {
-                data.splice(j, 1);
-                data[i][1]++;
-                continue;
+        dimension = data.length;
+
+        data = data.sort();
+
+        // перестраиваем выборку в вариационный ряд
+        for (i = 0; i < data.length; i++) {
+            tmp = data[i];
+            data[i] = [];
+            data[i].push(tmp);
+            data[i].push(1);
+
+            // убираем найденные равные значения
+            j = i + 1;
+            while (j < data.length) {
+                if (data[i][0] == data[j]) {
+                    data.splice(j, 1);
+                    data[i][1]++;
+                    continue;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        tmp = 0;
+        // выводим результаты в таблицу
+        for (i = 0, len = tmpArr.length; i < len; i++) {
+            if (i < data.length) {
+
+                if (i == 0) {
+                    tmp = 0;
+                } else {
+                    tmp += data[i - 1][1] / dimension;
+                }
+
+                $("#table").append("<tr><td>" + (i + 1) + "</td><td>" + tmpArr[i] + "</td>"
+                    + "<td>" + data[i][0] + "</td>" + "<td>" + data[i][1] + "</td>" + "<td>"
+                    + (data[i][1] / dimension).toFixed(4) + "</td><td>" + tmp.toFixed(4) + "</td></tr>");
+
             } else {
-                j++;
+                $("#table").append("<tr><td>" + (i + 1) + "</td><td>" + tmpArr[i] + "</td></tr>");
             }
         }
     }
 
-    // выводим результаты в таблицу
-    for (var i = 0, len = tmpArr.length; i < len; i++) {
-        if (i < data.length) {
-            $("#table").append("<tr><td>" + (i + 1) + "</td><td>" + tmpArr[i] + "</td>"
-            + "<td>" + data[i][0] + "</td>" + "<td>" + data[i][1] + "</td>" + "<td>"
-                + (data[i][1] / dimension) + "</td></tr>");
-        } else {
-            $("#table").append("<tr><td>" + (i + 1) + "</td><td>" + tmpArr[i] + "</td></tr>");
+    //
+    window.$("#buildClasses").click(function () { debugger;
+        var numberClasses = 0;
+        if ((dimension % 2) != 0) {
+            numberClasses++;
         }
-    }
-}
+        if (dimension <= 100) {
+            numberClasses = Math.trunc(Math.sqrt(dimension));
+        } else {
+            numberClasses = Math.trunc(Math.cbrt(dimension));
+        }
+        alert(numberClasses);
+        alert(data);
+    });
+
+})();
